ecc82175cf5e6d1ef78143c23dd243da58928fee
Ankit0427 <153299876+Ankit0427@users.noreply.github.com>
2025-02-24 13:19:41 +0530

    [Ankit Kumar] adding UC4 files

---
+++ src/main/java/com/example/Spring/intro/APIController.java
@@ -0,0 +1,32 @@
+package com.example.Spring.intro;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class APIController {
+    //Create a Rest Controller to demonstrate the various HTTP Methods and respond
+    //hello messages to the User. To begin
+    //with show Hello from BridgeLabz.
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello from BridgeLabz";
+    }
+
+    //Make REST Call to show Hello {name} from BridgeLabz
+    @GetMapping("/hello/query")
+    public String query(@RequestParam("name") String mark){
+        return "Hello "+mark+" from Bridgelabz";
+    }
+
+    @GetMapping("/hello/param/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello "+name+" from Bridgelabz";
+    }
+
+    @PostMapping("/hello/post")
+    public String greetUser(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+
+
+}
